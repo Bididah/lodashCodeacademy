b5095b7d0f2d7adb136e83b2a4fada34b6afad3b
ENEMIE <hanfihamza20@.gmail.com>
2021-01-28 23:07:06 +0100

    _.clamp

--- lodash/_.js
+++ lodash/_.js
@@ -1,4 +1,9 @@
-console.log("hamza")
+const _ = {clamp : (number , lower , upper ) => {
+ let  m  = Math.max(number,lower)
+ let n = Math.min(m ,upper)
+ return n ; 
+}} ; 
+
 
 // Do not write or modify code below this line.
 module.exports = _;
