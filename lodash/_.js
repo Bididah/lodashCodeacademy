01c41e5920aee1d245e19ff9a83149cc7c9606fb
ENEMIE <hanfihamza20@.gmail.com>
2021-01-29 23:21:36 +0100

    inRange()

--- lodash/_.js
+++ lodash/_.js
@@ -1,9 +1,31 @@
-const _ = {clamp : (number , lower , upper ) => {
+const _ = {
+clamp : (number , lower , upper ) => {
  let  m  = Math.max(number,lower)
  let n = Math.min(m ,upper)
  return n ; 
-}} ; 
+}, 
+inRange : (number , StartValue , endValue ) => {
+    let  n = StartValue ; 
+    let  m = endValue ;
+    if (endValue === undefined){
+        m = n  ; 
+        n = 0 ; 
+    }
+    if( n > m ) {
+        m = StartValue;
+        n = endValue ; 
+    }
+   if( number < n || number >= m ) {
+       console.log(m)
+       return false ; 
+   } else {
+       console.log(m)
+       return true ; 
+   }
+}
+} ;
 
+console.log(_.inRange( 6 , 10 ) ) 
 
 // Do not write or modify code below this line.
 module.exports = _;
