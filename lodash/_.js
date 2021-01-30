6945a4955b0a4c140fd4f16c0e2a09fb2a4aaa67
ENEMIE <hanfihamza20@.gmail.com>
2021-01-30 14:15:29 +0100

    .pad()

--- lodash/_.js
+++ lodash/_.js
@@ -26,8 +26,31 @@ inRange : (number , StartValue , endValue ) => {
 words : (string) => {
     const array = string.split(" ")
     return array ; 
+}, 
+pad : (string , length) => {   
+   let n = string.length
+   if(length <= n){
+       return string 
+   }else {
+       let totalPadding = length - n 
+       if (totalPadding%2 == 0 ){
+        for( let i = 0 ; i <(totalPadding/2) ; i++) {
+            string = " "+string
+            string = string+" "
+         }
+         return string ;
+       }else{
+        for( let i = 0 ; i <Math.floor(totalPadding/2) ; i++) {
+            string = " "+string
+         } 
+         for( let i = 0 ; i < Math.ceil((totalPadding/2))  ; i++) {
+            string = string+" "
+         }
+         return string ; 
+       }  }
 }
-} ;
+}
+
 
 // Do not write or modify code below this line.
 module.exports = _;
