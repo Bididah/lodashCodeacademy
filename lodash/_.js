c77f01b47d564899e8ecf3937a61e3a6a43c2013
ENEMIE <hanfihamza20@.gmail.com>
2021-02-03 16:32:27 +0100

    chunk

--- lodash/_.js
+++ lodash/_.js
@@ -81,8 +81,45 @@ dropWhile : ( array , func) => {
     let n = valueArray.indexOf(false)
     return _.drop(array,n)
 },
+
+chunk : (array , size ) => { 
+    let arr = []
+    let l = array.length
+    if (size === undefined ) {
+    
+        arr = array.map((e)=> {
+            if (array.indexOf(e) === l-1) {
+                return array.slice(array.indexOf(e))
+            } else{
+                return  array.slice((array.indexOf(e)),(-l+array.indexOf(e)+1))
+            }
+         
+        })
+
+        return arr 
+    }   
+    
+    let m = l%size 
+     for (let i = 0 ; i<l ; i++ ){
+         
+         if ( i%size == 0 ){
+            if(l-(i+size)> 0){
+                 arr.push(array.slice(i,-l+i+size))
+            } else {
+                if (l-(i+size) === 0) {
+                    arr.push(array.slice(i))
+                } else {
+                    arr.push(array.slice(l-m))
+                }
+                
+            }
+     } } return arr
+    }
 }
 
+    
+
+console.log(_.chunk(['a', 'b', 'c', 'd','e',"f","g","r","z","f"], 3))
 
 
 // Do not write or modify code below this line.
